722571258e9be8b098f5cc7a1490c2caf1f64e9d
Nguyen Hoang Quoc Bao <102313765+Nguyen-Hoang-Quoc-Bao@users.noreply.github.com>
2022-04-01 18:13:00 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-//login
-const login = document.querySelector('.js-login')
-const formlogin = document.querySelector('.formlogin')
-const closelogin = document.querySelector('.login--close')
-const btnLogin = document.querySelector('.login-btn')
-login.addEventListener('click', function(){
+//login-home
+const login = document.querySelector('.js-login');
+const formlogin = document.querySelector('.formlogin');
+const closelogin = document.querySelector('.login--close');
+const btnLogin = document.querySelector('.login-btn');
+    login.addEventListener('click', function(){
     formlogin.classList.add('openlogin')
 })
-closelogin.addEventListener('click', function(){
-    formlogin.classList.toggle('openlogin')
-})
-btnLogin.onclick = function(){
+    closelogin.addEventListener('click', function(){
+    formlogin.classList.toggle('openlogin');
+})  
+    btnLogin.onclick = function(){
         alert('Đăng nhập thành công');
         formlogin.classList.remove('openlogin');
 }
-//signup
+//signup-home
 const signup = document.querySelector('.js-signup')
 const formsignup = document.querySelector('.formsignup')
 const closesignup = document.querySelector('.login-close')
-signup.addEventListener('click', function(){
-    formsignup.classList.add('opensignup')
+const btnSignup = document.querySelector('.btn-signup')
+    signup.addEventListener('click', function(){
+    formsignup.classList.add('opensignup');
 })
-signup.addEventListener('click', function(){
-    formlogin.classList.remove('openlogin')
+    signup.addEventListener('click', function(){
+    formlogin.classList.remove('openlogin');
 })
-closesignup.addEventListener('click', function(){
-    formsignup.classList.toggle('opensignup')
+    closesignup.addEventListener('click', function(){
+    formsignup.classList.toggle('opensignup');
 })
-
-//header
+    btnSignup.onclick = function(){
+        alert('Đăng ký thành công');
+        formsignup.classList.toggle('opensignup');
+    }
+//header-home
 const homemenu = document.getElementById('moblie-navbar');
 const header = document.getElementById('header-0');
 homemenu.onclick = function(){
@@ -39,15 +43,15 @@ homemenu.onclick = function(){
         header.style.height = '49px';
     }
 }
-
-const menuUser = document.getElementById('moblie-navbar');
-const headeruser = document.getElementById('header-0');
-menuUser.onclick = function(){
-    var isOpen = headeruser.clientHeight === 49;
-    if(isOpen) {
-        headeruser.style.height = 'auto';
-    }
-    else{
-        headeruser.style.height = '49px';
-    }
-}
+//header-user
+// const menuUser = document.getElementById('moblie-navbar');
+// const headeruser = document.getElementById('header-0');
+// menuUser.onclick = function(){
+//     var isOpen = headeruser.clientHeight === 49;
+//     if(isOpen) {
+//         headeruser.style.height = 'auto';
+//     }
+//     else{
+//         headeruser.style.height = '49px';
+//     }
+// }
